18aef20912c56d890d6a82796815d89aa574f70d
hklinhai <linh@bjhxqh.com>
2018-01-18 15:53:24 +0800

    modify

--- src/test/java/com/hxqh/crawler/test/EmployeeCRUDAppTest.java
+++ src/test/java/com/hxqh/crawler/test/EmployeeCRUDAppTest.java
@@ -21,81 +21,81 @@ public class EmployeeCRUDAppTest {
 
     private TransportClient client;
 
-    @Before
-    public void init() throws Exception {
-        Settings settings = Settings.builder()
-                .put("cluster.name", "elasticsearch")
-                .build();
+//    @Before
+//    public void init() throws Exception {
+//        Settings settings = Settings.builder()
+//                .put("cluster.name", "elasticsearch")
+//                .build();
+//
+//        client = new PreBuiltTransportClient(settings)
+//                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+//
+////		createEmployee(client);
+////		getEmployee(client);
+////		updateEmployee(client);
+////		deleteEmployee(client);
+//
+////        client.close();
+//    }
 
-        client = new PreBuiltTransportClient(settings)
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
-
-//		createEmployee(client);
-//		getEmployee(client);
-//		updateEmployee(client);
-//		deleteEmployee(client);
-
+//    /**
+//     * 创建员工信息（创建一个document）
+//     */
+//    @Test
+//    public void createEmployee() throws Exception {
+//        IndexResponse response = client.prepareIndex("company", "employee", "1")
+//                .setSource(XContentFactory.jsonBuilder()
+//                        .startObject()
+//                        .field("name", "jack")
+//                        .field("age", 27)
+//                        .field("position", "technique")
+//                        .field("country", "china")
+//                        .field("join_date", "2017-01-01")
+//                        .field("salary", 10000)
+//                        .endObject())
+//                .get();
+//        System.out.println(response.getResult());
 //        client.close();
-    }
-
-    /**
-     * 创建员工信息（创建一个document）
-     */
-    @Test
-    public void createEmployee() throws Exception {
-        IndexResponse response = client.prepareIndex("company", "employee", "1")
-                .setSource(XContentFactory.jsonBuilder()
-                        .startObject()
-                        .field("name", "jack")
-                        .field("age", 27)
-                        .field("position", "technique")
-                        .field("country", "china")
-                        .field("join_date", "2017-01-01")
-                        .field("salary", 10000)
-                        .endObject())
-                .get();
-        System.out.println(response.getResult());
-        client.close();
-    }
-
-    /**
-     * 获取员工信息
-     *
-     * @throws Exception
-     */
-    @Test
-    public void getEmployee() throws Exception {
-        GetResponse response = client.prepareGet("company", "employee", "1").get();
-        System.out.println(response.getSourceAsString());
-    }
-
-
-    /**
-     * 修改员工信息
-     *
-     * @throws Exception
-     */
-    @Test
-    public void updateEmployee() throws Exception {
-        UpdateResponse response = client.prepareUpdate("company", "employee", "1")
-                .setDoc(XContentFactory.jsonBuilder()
-                        .startObject()
-                        .field("position", "technique manager")
-                        .endObject())
-                .get();
-        System.out.println(response.getResult());
-    }
-
-
-    /**
-     * 删除 员工信息
-     *
-     * @throws Exception
-     */
-    @Test
-    public void deleteEmployee() throws Exception {
-        DeleteResponse response = client.prepareDelete("company", "employee", "1").get();
-        System.out.println(response.getResult());
-    }
+//    }
+//
+//    /**
+//     * 获取员工信息
+//     *
+//     * @throws Exception
+//     */
+//    @Test
+//    public void getEmployee() throws Exception {
+//        GetResponse response = client.prepareGet("company", "employee", "1").get();
+//        System.out.println(response.getSourceAsString());
+//    }
+//
+//
+//    /**
+//     * 修改员工信息
+//     *
+//     * @throws Exception
+//     */
+//    @Test
+//    public void updateEmployee() throws Exception {
+//        UpdateResponse response = client.prepareUpdate("company", "employee", "1")
+//                .setDoc(XContentFactory.jsonBuilder()
+//                        .startObject()
+//                        .field("position", "technique manager")
+//                        .endObject())
+//                .get();
+//        System.out.println(response.getResult());
+//    }
+//
+//
+//    /**
+//     * 删除 员工信息
+//     *
+//     * @throws Exception
+//     */
+//    @Test
+//    public void deleteEmployee() throws Exception {
+//        DeleteResponse response = client.prepareDelete("company", "employee", "1").get();
+//        System.out.println(response.getResult());
+//    }
 
 }
--- src/test/java/com/hxqh/crawler/test/URLTest.java
+++ src/test/java/com/hxqh/crawler/test/URLTest.java
@@ -8,10 +8,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -24,10 +24,10 @@ import java.util.regex.Pattern;
 @SpringBootTest
 public class URLTest {
 
-    private static final Integer PAGE_NUM = 30;
+    private static final Integer PAGE_NUM = 10;
     private static final String URL = "<a data-searchpingback-elem=\"link\" data-searchpingback-param=\"target=3964b0adbecf7fe307271420f47339a6&amp;ptype=1&amp;site=iqiyi&amp;pos=1\" rseat=\"bigTitle\" title=\"前任2：备胎反击战\" href=\"http://www.iqiyi.com/v_19rrkih1u4.html#vfrm=2-4-0-1\" target=\"_blank\">前任2：备胎反击战</a>\n";
 
-    @Resource
+    @Autowired
     private CrawlerURLRepository crawlerURLRepository;
 
     /**
@@ -78,8 +78,6 @@ public class URLTest {
         }
 
         // 3.解析
-
-
         List<CrawlerURL> crawlerURLS = new ArrayList<>();
         // . 取每个页面的需要持久化URL
         for (String s : hrefList) {
