1c5c7023a33d80c77a9eb8104b55cb47dc9b68c3
hklinhai <hkhai@outlook.com>
2018-02-06 10:52:31 +0800

    修正删除

---
+++ src/main/java/com/hxqh/crawler/service/CrawlerService.java
@@ -0,0 +1,14 @@
+package com.hxqh.crawler.service;
+
+/**
+ * Created by Ocean lin on 2018/2/6.
+ *
+ * @author Ocean lin
+ */
+
+public interface CrawlerService {
+
+    void deleteIqiyiFilm();
+
+    void delTencentFilm();
+}
