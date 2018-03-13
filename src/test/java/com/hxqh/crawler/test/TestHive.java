66ab04889bc06145679333f09c0a7249f1da546f
hklinhai <hkhai@outlook.com>
2018-03-13 17:21:52 +0800

    modify test & pom

--- src/test/java/com/hxqh/crawler/test/TestHive.java
+++ src/test/java/com/hxqh/crawler/test/TestHive.java
@@ -17,90 +17,91 @@ public class TestHive {
     private PreparedStatement ps;
     private ResultSet rs;
 
-
-    //创建连接
-    @Before
-    public void getConnection() {
-        try {
-            Class.forName("org.apache.hadoop.hive.jdbc.HiveDriver");
-            connection = DriverManager.getConnection("jdbc:hive://192.168.1.167:10000/hive", "", "");
-            System.out.println(connection);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //关闭连接
-    @After
-    public void close() {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-            if (ps != null) {
-                ps.close();
-            }
-            if (connection != null) {
-                connection.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // 创建表
-    @Test
-    public void createTable() {
-//        String sql = "create table goods2(id int,name string) ";
-        String sql = "desc students";
-        try {
-            ps = connection.prepareStatement(sql);
-            ps.execute(sql);
-            close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // 删除表
-    @Test
-    public void dropTable() {
-        String sql = "drop table goods";
-        try {
-            ps = connection.prepareStatement(sql);
-            ps.execute();
-            close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //添加数据
-    @Test
-    public void insert() throws SQLException {
-        String filepath = "/videos/2018/03/2018-03-07-iqiyi";
-        String tableName = "TEST_LOAD_2";
-        String sql = "load data inpath '" + filepath + "' into table " + tableName;
-        Statement stmt = connection.createStatement();
-        stmt.execute(sql);
-        close();
-    }
-
-    //查询
-    @Test
-    public void find() throws SQLException {
-//        String sql = "select * from students ";
-        String sql = "select * from test_load_2 limit 10 ";
-
-        ps = connection.prepareStatement(sql);
-        rs = ps.executeQuery();
-        while (rs.next()) {
-            System.out.println(rs.getObject(1) + "---" + rs.getObject(2));
-        }
-        close();
-    }
+//
+//    //创建连接
+//    @Before
+//    public void getConnection() {
+//        try {
+//            Class.forName("org.apache.hadoop.hive.jdbc.HiveDriver");
+//            connection = DriverManager.getConnection("jdbc:hive://192.168.1.167:10000/hive", "", "");
+//            System.out.println(connection);
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    //关闭连接
+//    @After
+//    public void close() {
+//        try {
+//            if (rs != null) {
+//                rs.close();
+//            }
+//            if (ps != null) {
+//                ps.close();
+//            }
+//            if (connection != null) {
+//                connection.close();
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    // 创建表
+//    @Test
+//    public void createTable() {
+////        String sql = "create table goods2(id int,name string) ";
+//        String sql = "desc students";
+//        try {
+//            ps = connection.prepareStatement(sql);
+//            ps.execute(sql);
+//            close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    // 删除表
+//    @Test
+//    public void dropTable() {
+//        String sql = "drop table goods";
+//        try {
+//            ps = connection.prepareStatement(sql);
+//            ps.execute();
+//            close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    //添加数据
+//    @Test
+//    public void insert() throws SQLException {
+//        String filepath = "/videos/2018/03/2018-03-05-iqiyi";
+//        String tableName = "test_load_2";
+//        String sql = "load data inpath '" + filepath + "' into table " + tableName;
+//        System.out.println(sql);
+//        Statement stmt = connection.createStatement();
+//        stmt.execute(sql);
+//        close();
+//    }
+//
+//    //查询
+//    @Test
+//    public void find() throws SQLException {
+////        String sql = "select * from students ";
+//        String sql = "select * from test_load_2 limit 10 ";
+//
+//        ps = connection.prepareStatement(sql);
+//        rs = ps.executeQuery();
+//        while (rs.next()) {
+//            System.out.println(rs.getObject(1) + "---" + rs.getObject(2));
+//        }
+//        close();
+//    }
 
 
 }
