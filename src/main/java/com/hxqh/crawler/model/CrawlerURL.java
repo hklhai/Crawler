ffb4d1e2cd22a5dc8baf52371045b1ffec21346c
hklinhai <hkhai@outlook.com>
2018-03-30 11:04:53 +0800

    修正

--- src/main/java/com/hxqh/crawler/model/CrawlerURL.java
+++ src/main/java/com/hxqh/crawler/model/CrawlerURL.java
@@ -1,6 +1,7 @@
 package com.hxqh.crawler.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,6 +13,7 @@ import javax.persistence.Table;
 public class CrawlerURL {
 
     @Id
+    @GeneratedValue
     private Integer cid;
     private String title;
     private String url;
