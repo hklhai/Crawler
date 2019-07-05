edca3fe98e196f7c0c858e915ecda2613bedac6b
hklinhai <hklhai@163.com>
2019-07-05 09:55:14 +0800

    电影URL

--- src/main/java/com/hxqh/crawler/controller/IqiyiController.java
+++ src/main/java/com/hxqh/crawler/controller/IqiyiController.java
@@ -2,6 +2,7 @@ package com.hxqh.crawler.controller;
 
 import com.hxqh.crawler.common.Constants;
 import com.hxqh.crawler.controller.thread.PersistFilm;
+import com.hxqh.crawler.domain.URLInfo;
 import com.hxqh.crawler.model.CrawlerSoapURL;
 import com.hxqh.crawler.model.CrawlerURL;
 import com.hxqh.crawler.model.CrawlerVariety;
@@ -9,6 +10,7 @@ import com.hxqh.crawler.model.CrawlerVarietyURL;
 import com.hxqh.crawler.repository.*;
 import com.hxqh.crawler.service.CrawlerService;
 import com.hxqh.crawler.service.SystemService;
+import com.hxqh.crawler.util.CrawlerUtils;
 import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +21,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.stream.Collectors;
@@ -58,6 +57,57 @@ public class IqiyiController {
 
     @RequestMapping("/filmUrl")
     public String filmUrl() throws Exception {
+
+        /**
+         * 爬取数据
+         */
+        // 1.所有待爬取URLList
+        Map<String, URLInfo> allStartURLMap = new HashMap<>();
+        Map<String, String> prefixSuffixMap = new HashMap<>();
+        Map<String, URLInfo> hrefMap = new HashMap<>();
+
+        // 2.获取全部页面Url
+        prefixSuffixMap.put("http://list.iqiyi.com/www/1/-------------11-", "-1-iqiyi--.html|iqiyi|film|hot");
+        prefixSuffixMap.put("http://list.iqiyi.com/www/1/-------------4-", "-1-iqiyi--.html|iqiyi|film|new");
+        prefixSuffixMap.put("http://list.iqiyi.com/www/1/-------------8-", "-1-iqiyi--.html|iqiyi|film|score");
+
+        for (Map.Entry<String, String> entry : prefixSuffixMap.entrySet()) {
+            String prefix = entry.getKey();
+            String[] split = entry.getValue().split("\\|");
+            String suffix = split[0];
+            String platform = split[1];
+            String category = split[2];
+            String sorted = split[3];
+
+            URLInfo urlInfo = new URLInfo(platform, category, sorted);
+
+            for (int i = Constants.PAGE_START_NUM; i <= Constants.PAGE_END_NUM; i++) {
+                String url = prefix + i + suffix;
+                allStartURLMap.put(url, urlInfo);
+            }
+        }
+
+        for (Map.Entry<String, URLInfo> entry : allStartURLMap.entrySet()) {
+            String url = entry.getKey();
+            URLInfo urlInfo = entry.getValue();
+            try {
+                String outerHTML = CrawlerUtils.fetchHTMLContentByPhantomJs(url, Constants.DEFAULT_SEELP_SECOND);
+
+                String[] split = outerHTML.split("\n");
+                for (int i = 0; i < split.length; i++) {
+                    String href = CrawlerUtils.getHref(split[i]);
+                    if (href != null && href.contains("vfrm=2-4-0-1")) {
+                        // 写入ElasticSerach
+                        systemService.addFilmOrSoapUrl(href, urlInfo);
+                        hrefMap.put(href, urlInfo);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        crawlerService.persistFilmUrl(hrefMap);
+
 
 
         return "crawler/notice";
